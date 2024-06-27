68172e44bde59fa406e948e57a6de8332c279385
Brian Nderitu <raulkenyonis007@gmail.com>
2024-06-27 13:35:39 +0300

    added number.py

---
+++ challenge.py/numbers.py
@@ -0,0 +1,13 @@
+# create a list of numbers
+numbers = [1, 5, 3, 9]
+
+# initialize the variable to 0
+variable = 0 
+total = 0
+# Use a for loop to iterate over the numbers list
+for number in numbers:
+    #add the current number to total variable
+    total += number 
+
+#print the final total value
+print("the sum of all the numbers in the list is:", total)
